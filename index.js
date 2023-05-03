b2beffb34673723fcb9d4cc32022849737b215af
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2023-05-03 11:33:56 -0400

    created a server without express

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const fs = require('fs');
 
 const server= http.createServer((req,res)=>{
  res.statusCode = 200;
+ //set header content type
  res.setHeader('Content-Type', 'text/plain');
- fs.readFile('./views/index.html', (err, data)=>{
+
+ let path = './views/';
+ switch (req.url){
+  case '/':
+    path += 'index.html';
+    break;
+  case '/about':
+    path += 'about.html';
+    break;
+ }
+
+ fs.readFile(path, (err, data)=>{
   if (err) {
     console.log(err)
    } else {
-    res.write(data);
-    res.end();
+    res.end(data);
    }
  });
 });
